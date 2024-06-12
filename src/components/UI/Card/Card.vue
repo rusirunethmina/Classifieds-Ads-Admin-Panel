7b31f0b02fdf2dc4d4231191edab0b0c9663a280
rusirunethmina <rusiruc21@gmail.com>
2024-06-12 17:31:03 +0400

    add new folders

---
+++ src/components/UI/Card/Card.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <div class="flex items-center bg-white border rounded-sm overflow-hidden shadow">
+            <div class="p-4 bg-blue-400">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-12 w-12 text-white" fill="none" viewBox="0 0 24 24"
+                    stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="M8 7v8a2 2 0 002 2h6M8 7V5a2 2 0 012-2h4.586a1 1 0 01.707.293l4.414 4.414a1 1 0 01.293.707V15a2 2 0 01-2 2h-2M8 7H6a2 2 0 00-2 2v10a2 2 0 002 2h8a2 2 0 002-2v-2" />
+                </svg>
+            </div>
+            <div class="px-4 text-gray-700">
+                <h3 class="text-sm tracking-wider">{{ title }}</h3>
+                <p class="text-3xl">{{ value }}</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Card',
+}
+</script>
---
+++ src/components/UI/Card/CardChart.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <div class="px-4 py-2 bg-white border rounded-md overflow-hidden shadow">
+            <h3 class="text-xl text-gray-600 mb-4">{{ title }}</h3>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'CardChart',
+}
+</script>
